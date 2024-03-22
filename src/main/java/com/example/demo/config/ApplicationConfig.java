cb9afce48919174f1c6b8dc3467b7ef8b6e4986e
Emir Zambakovic <83666062+zexach@users.noreply.github.com>
2024-03-22 23:46:16 +0100

    Fixed compiling error

--- src/main/java/com/example/demo/config/ApplicationConfig.java
+++ src/main/java/com/example/demo/config/ApplicationConfig.java
@@ -36,8 +36,7 @@ public class ApplicationConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager()
-    }
+        return config.getAuthenticationManager();    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
